f29984b4d43ac8680bf25b4144ffd0949354c5f2
Taekyung Lee <terry@terryui-MacBook-Pro.local>
2019-02-20 18:24:38 +0900

    project initialize

---
+++ src/test/java/com/tklee/study/inflearnrestapi/InflearnRestApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tklee.study.inflearnrestapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InflearnRestApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
