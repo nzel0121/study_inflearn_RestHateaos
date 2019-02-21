b33eb445f64e841ee467810e17f6303427bcf1bc
Taekyung Lee <terry@terryui-MacBook-Pro.local>
2019-02-21 14:01:43 +0900

    resolved #1 create Event object for content , and issue close test

--- src/test/java/com/tklee/study/inflearnrestapi/events/EventControllerTests.java
+++ src/test/java/com/tklee/study/inflearnrestapi/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package com.tklee.study.inflearnrestapi.events;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -22,11 +24,29 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void createEvent() throws Exception {
+        //Given
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Develoment with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019,02,21,14,00))
+                .endEnrollmentDateTime(LocalDateTime.of(2019,02,21,14,00))
+                .beginEventDateTime(LocalDateTime.of(2019,02,21,14,00))
+                .endEventDateTime(LocalDateTime.of(2019,02,21,14,00))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("가양역 우림비즈나인")
+                .build();
+
         mockMvc.perform(post("/api/events/")
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .accept(MediaTypes.HAL_JSON))
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
                 .andExpect(status().isCreated());
     }
